0c48150b75eac4d18e55317ca7afaf0ba6534d62
Yann-Edern Gillet <helloyannglt@gmail.com>
2022-03-07 22:26:05 +0100

    🚧 Try fresh config with netlify functions

--- express/server.js
+++ express/server.js
@@ -1,9 +1,11 @@
+'use strict'
 const express = require('express');
+const serverless = require('serverless-http');
 const puppeteer = require('puppeteer');
-const Handlebars = require("handlebars");
+const handlebars = require("handlebars");
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 const templateHTML = `
   <!DOCTYPE html>
@@ -23,44 +25,25 @@ const templateHTML = `
               <text fill="#FF715C" xml:space="preserve" style="white-space: pre" font-family="Fira Code" font-size="24" font-weight="600"><tspan text-anchor="middle" x="150" y="108">{{measurement}}</tspan></text>
               <rect x="64" y="58" width="172" height="84" rx="4" stroke="#FF715C" stroke-width="2" stroke-dasharray="8 4"/>
             </g>
-          <defs>
-            <filter id="filter0_dd_226_16959" x="32" y="28" width="236" height="148" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-              <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-              <feOffset dy="2"/>
-              <feGaussianBlur stdDeviation="4"/>
-              <feComposite in2="hardAlpha" operator="out"/>
-              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.04 0"/>
-              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_226_16959"/>
-              <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-              <feOffset dy="1"/>
-              <feGaussianBlur stdDeviation="2"/>
-              <feComposite in2="hardAlpha" operator="out"/>
-              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.04 0"/>
-              <feBlend mode="normal" in2="effect1_dropShadow_226_16959" result="effect2_dropShadow_226_16959"/>
-              <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_226_16959" result="shape"/>
-            </filter>
-          </defs>
         </svg>
-
       </main>
     </body>
   </html>
 `;
 
-const templateStyles = `
-  * {
-    box-sizing: border-box;
-  }
-`;
+app.use('/.netlify/functions/server', router) // path must route to lambda
+app.use('/', router)
 
-app.get('/ogimage', async (req, res) => {
-  const compiledStyles = Handlebars.compile(templateStyles);
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write('<h1>Hello, stranger</h1>')
+  res.end()
+})
 
+router.get('/ogimage', async (req, res) => {
   // compiled HTML
-  const compiledHTML = Handlebars.compile(templateHTML)({
+  const compiledHTML = handlebars.compile(templateHTML)({
     measurement: req.query.measurement,
-    styles: compiledStyles,
   });
 
   // Launch Headless browser and capture creenshot
@@ -85,4 +68,5 @@ app.get('/ogimage', async (req, res) => {
   res.end(image);
 });
 
-app.listen(port, () => console.log(`Server is running in port ${port}`));
+module.exports = app
+module.exports.handler = serverless(app)
