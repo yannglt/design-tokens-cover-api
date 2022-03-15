d1240fb3b943b77ede1aaa9f928bd8e3f4ea5e3b
Yann-Edern Gillet <helloyannglt@gmail.com>
2022-03-15 21:09:03 +0100

    ➕ Add sharp

--- express/server.js
+++ express/server.js
@@ -1,9 +1,16 @@
-'use strict'
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
+const sharp = require('sharp');
+
+const app = express();
 const router = express.Router();
+
+const source = `
+<svg height="100" width="100">
+  <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+</svg>
+`;
 
 app.use(bodyParser.json())
 app.use('/.netlify/functions/server', router)
@@ -15,12 +22,22 @@ router.get('/', (req, res) => {
   res.end()
 })
 
-// router.get('/ogimage', async (req, res) => {
-  
+router.get('/ogimage', async (req, res) => {
 
-  // res.writeHead(200, { 'Content-Type': 'image/png' });
-  // res.end(image);
-// });
+  const image = await sharp({
+    create: {
+      width: 48,
+      height: 48,
+      channels: 4,
+      background: { r: 255, g: 0, b: 0, alpha: 1 }
+    }
+  })
+  .png()
+  .toBuffer();
+
+  res.writeHead(200, { 'Content-Type': 'image/png' });
+  res.end(image);
+});
 
 module.exports = app
 module.exports.handler = serverless(app)
